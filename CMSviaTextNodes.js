e70b35f6ff4d041234a4aca99a8a907527faa12e
voytecu314 <35760560+voytecu314@users.noreply.github.com>
2023-10-05 20:03:48 +0200

    Create CMSviaTextNodes.js
    CMS via Text Nodes v 1.0.1

---
+++ CMSviaTextNodes.js
@@ -0,0 +1,89 @@
+function isTextNode (node) {
+
+    //is text node
+    if (node.nodeType !== 3) {
+        return false;   
+    }
+
+    //is first char enter and any other is not enter or space
+    if (node.textContent.charCodeAt(0) === 10) {
+        for(let i=1; i<node.textContent.length;i++){
+            if(node.textContent.charCodeAt(i) !==10 && node.textContent.charCodeAt(i) !==32 ){
+                return true;
+            }
+        }    
+        return false;    
+    }
+
+    //is first char space and any other is not enter or space
+    if (node.textContent.charCodeAt(0) === 32) {
+        for(let i=1; i<node.textContent.length;i++){
+            if(node.textContent.charCodeAt(i) !==10 && node.textContent.charCodeAt(i) !==32 ){
+                return true;
+            }
+        }    
+        return false;    
+    }
+
+    return true;
+
+}
+
+function detectTextContent (element,eventType) {
+    
+    const nodes = element.childNodes;
+
+    let parentIsSet = false;
+
+    for(let node of nodes) {
+        //if(eventType==='click') return node.textContent;
+
+        if( (!parentIsSet) && isTextNode(node) ) { 
+
+            node.parentElement.classList.add('admin-edit');
+            node.parentElement.addEventListener('click', editText);
+
+            parentIsSet=true;
+
+        }
+
+        if(node.nodeType === 1) {
+            detectTextContent(node);
+        } 
+    }
+}
+
+let HTMLfromPreviousField, previousField;
+
+function editText (event) {
+    event.stopImmediatePropagation();
+    event.preventDefault();
+    if(event.target.tagName==='I' || event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') return;
+    //const text = detectTextContent(event.target,event.type);
+    const textArray = [];
+    const loop = event.target.childNodes.length;
+    for(let i=0;i<loop;i++){
+        textArray.push(event.target.childNodes[i].textContent);
+    };
+    previousHTML = event.target.innerHTML;
+
+    if(previousField) previousField.innerHTML=HTMLfromPreviousField;
+    HTMLfromPreviousField = event.target.innerHTML;
+    previousField = event.target;
+
+    event.target.innerHTML = '';
+    for(let i = 0; i < loop; i++) {
+        const id = 'id'+crypto.randomUUID();
+        event.target.innerHTML += textArray[i].length > 50 ? 
+                                `<textarea cols='25' rows='5' placeholder='${textArray[i]}' id='${id}' style='all: revert' />`
+                                :`<input type='text' value='${textArray[i]}' id='${id}' style='all: revert' />`;
+        //document.querySelector(`#${id}`).addEventListener('mouseleave',(e)=>resetElement(document.querySelector(`#${id}`),previousHTML));
+        document.querySelector(`#${id}`).select();
+    };
+}
+
+/* function resetElement (target, HTML) {
+    target.parentElement.innerHTML = HTML;
+} */
+
+detectTextContent(document.body, 'load');
